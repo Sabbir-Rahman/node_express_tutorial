3cb250bbeda5738afa433c94baf7ff67469af2a8
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-04-22 03:33:32 +0600

    module setup

--- 1-intro.js
+++ 1-intro.js
@@ -1,5 +1,9 @@
-// GLOBALS  - NO WINDOW !!!
-// __dirname    - path to current directory
-// require      - function to use module (file)
-// module       - into about current module (file)
-// process      - info about env where the program is being executed
+const amount = 9
+
+if(amount<10){
+    console.log('small number')
+} else {
+    console.log('large number')
+}
+
+console.log('Hello node')
