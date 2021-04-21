893fab574a690ce5ee74494301013d9bbddb3964
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-04-22 03:12:53 +0600

    first

---
+++ 1-intro.js
@@ -0,0 +1,5 @@
+// GLOBALS  - NO WINDOW !!!
+// __dirname    - path to current directory
+// require      - function to use module (file)
+// module       - into about current module (file)
+// process      - info about env where the program is being executed
