3bc08b2923dcc20ba6f2198439ac1d1cb571d4c2
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-05-26 20:31:31 +0600

    middleware are placed differently

--- express-tutorial/8-string-query.js
+++ express-tutorial/8-string-query.js
@@ -1,6 +1,6 @@
+const {products} = require('./data')
 const express = require('express');
 const app = express()
-const {products} = require('./data')
 
 app.get('/',(req,res)=>{
     res.send('<h1>Home Page </h1><a href="/api/products">products')
@@ -56,8 +56,4 @@ app.get('/api/v1/query',(req,res)=>{
 app.get('/api/products/:productId/reviews/:reviewId', (req,res)=>{
     res.send('Hello World')
     // console.log(req.params)
-})
-
-app.listen(5000, ()=> {
-    console.log('Server is listening on port 5000....')
 })
--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,61 +1,22 @@
 const express = require('express');
 const app = express()
-const {products} = require('./data')
+const logger = require('./9-logger')
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Home Page </h1><a href="/api/products">products')
+
+//req => middleware => res
+
+
+app.get('/', logger,(req,res)=> {
+    
+    res.send('Home')
 })
 
-app.get('/api/products',(req,res)=>{
-
-    //make custom product json without the description
-    const newProducts = products.map((product)=>{
-        const {id,name,image} = product;
-        return {id,name,image}
-    })
-    // res.json(products)
-    res.json(newProducts)
-
+app.get('/api/products',logger,(req,res)=>{
+    res.send('Products')
 })
 
-app.get('/api/products/:productId', (req,res)=>{
-
-    const {productId} = req.params;
-
-    const singleProduct = products.find((product)=>product.id === Number(productId))
-
-    if(!singleProduct){
-        return res.status(404).send("product Does Not Exist")
-    }
-
-    return res.json(singleProduct)
-})
-
-//query string
-app.get('/api/v1/query',(req,res)=>{
-
-    const { search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if(search){
-        sortedProducts = sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if(sortedProducts.length < 1){
-        res.status(200).json({ success: true, data: []})
-    }
-    res.status(200).json(sortedProducts)
-    console.log(req.query);
-})
-
-
-app.get('/api/products/:productId/reviews/:reviewId', (req,res)=>{
-    res.send('Hello World')
-    // console.log(req.params)
+app.get('/api/items',logger,(req,res)=>{
+    res.send('Items')
 })
 
 app.listen(5000, ()=> {
