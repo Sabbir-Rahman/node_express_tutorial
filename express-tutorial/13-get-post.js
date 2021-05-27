14f18347e7bee2d51605e5c3c6450269114d9858
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-05-27 06:38:50 +0600

    get post method

--- express-tutorial/13-get-post.js
+++ express-tutorial/13-get-post.js
@@ -31,6 +31,16 @@ app.post('/login', (req, res) => {
     res.status(401).send('Please Provide Credentials')
 })
 
+app.post('/api/postman/people',(req,res)=>{
+    const { name } = req.body
+    if (!name) {
+        return res
+        .status(400)
+        .json({ success: false, msg: 'please provide name value'})
+    }
+    res.status(201).send({success:true, data: [...people]})
+})
+
 app.listen(5000, ()=> {
     console.log('Server is listening on port 5000....')
 })
